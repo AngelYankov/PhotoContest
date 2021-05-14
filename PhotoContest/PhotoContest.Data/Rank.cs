f9476671de85f4ff2e989d415aef982e75642cd0
Emil Nenchev <emilnenchev7@gmail.com>
2021-05-14 13:14:08 +0300

    create fan and rank class

---
+++ PhotoContest/PhotoContest.Data/Rank.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhotoContest.Data
+{
+    public class Rank
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Fan> Fans { get; set; } = new HashSet<Fan>();
+    }
+}
