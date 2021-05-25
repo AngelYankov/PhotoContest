e4f805f51fc980bf76751307ede3f5bf412ce8af
Emil Nenchev <emilnenchev7@gmail.com>
2021-05-25 19:24:49 +0300

    user config update

--- PhotoContest/PhotoContest.Data/Configurations/UserConfig.cs
+++ PhotoContest/PhotoContest.Data/Configurations/UserConfig.cs
@@ -15,11 +15,6 @@ namespace PhotoContest.Data.Configurations
             builder.Property(u => u.PasswordHash).IsRequired();
             builder.Property(u => u.UserName).IsRequired();
             builder.HasIndex(u => u.UserName).IsUnique();
-
-            builder.HasOne(u => u.)
-                .WithMany(r => r.Users)
-                .HasForeignKey(u => u.RankId)
-                .OnDelete(DeleteBehavior.NoAction);
         }
     }
 }
