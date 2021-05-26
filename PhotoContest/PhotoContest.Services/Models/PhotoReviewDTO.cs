d596bb6e44977829b7338dd7f7c7719a392e5a0b
Emil Nenchev <emilnenchev7@gmail.com>
2021-05-26 12:16:58 +0300

    Add class helper to show all info about score and comments

--- PhotoContest/PhotoContest.Services/Models/PhotoReviewDTO.cs
+++ PhotoContest/PhotoContest.Services/Models/PhotoReviewDTO.cs
@@ -1,4 +1,5 @@
 ﻿using PhotoContest.Data.Models;
+using PhotoContest.Services.Models.ReviewInfoHelper;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -12,13 +13,13 @@ namespace PhotoContest.Services.Models
             this.Title = photo.Title;
             this.Description = photo.Description;
             this.PhotoUrl = photo.PhotoUrl;
-            this.User = photo.User.FirstName+" "+photo.User.LastName;
+            this.User = photo.User.FirstName + " " + photo.User.LastName;
             this.Contest = photo.Contest.Name;
             this.Category = photo.Contest.Category.Name;
             this.Points = photo.AllPoints;
             foreach (var review in photo.Reviews)
             {
-                this.Reviews.Add((review.Comment,review.Score));
+                this.Reviews.Add(new InfoHelper() { Comment = review.Comment, Score = review.Score });
             }
         }
         public string Title { get; set; }
@@ -28,6 +29,6 @@ namespace PhotoContest.Services.Models
         public string Contest { get; set; }
         public string Category { get; set; }
         public double Points { get; set; }
-        public List<(string, double)> Reviews { get; set; } = new List<(string, double)>();
+        public List<InfoHelper> Reviews { get; set; } = new List<InfoHelper>();
     }
 }
---
+++ PhotoContest/PhotoContest.Services/Models/ReviewInfoHelper/InfoHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhotoContest.Services.Models.ReviewInfoHelper
+{
+    public class InfoHelper
+    {
+        public string Comment { get; set; }
+        public double Score { get; set; }
+    }
+}
