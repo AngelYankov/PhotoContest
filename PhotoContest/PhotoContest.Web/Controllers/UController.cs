1e1d7a282f37dbcb28b9d62a3798435fcf80f108
Emil Nenchev <emilnenchev7@gmail.com>
2021-05-28 17:50:06 +0300

    namespace update for user controller

--- PhotoContest/PhotoContest.Web/Controllers/UController.cs
+++ PhotoContest/PhotoContest.Web/Controllers/UController.cs
@@ -13,40 +13,34 @@ using PhotoContest.Services.Models.Update;
 
 namespace PhotoContest.Web.Controllers
 {
-    public class UsersController : Controller
+    public class UController : Controller
     {
         private readonly PhotoContestContext _context;
         private readonly IUserService userService;
 
-        public UsersController(PhotoContestContext context, IUserService userService)
+        public UController(PhotoContestContext context, IUserService userService)
         {
             _context = context;
             this.userService = userService;
         }
 
-        // GET: Users
         public async Task<IActionResult> Index()
         {
             var users = await this.userService.GetAllAsync();
             return View(users);
         }
 
-        // GET: Users/Details/5
         public async Task<IActionResult> Details(string username)
         {
             var user = await this.userService.GetUserByUsernameAsync(username);
             return View(new UserDTO(user));
         }
 
-        // GET: Users/Create
         public IActionResult Create()
         {
             return View();
         }
 
-        // POST: Users/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(NewUserDTO newUserDTO)
@@ -65,17 +59,36 @@ namespace PhotoContest.Web.Controllers
             }
             return View();
         }
+        public IActionResult CreateOrganizer()
+        {
+            return RedirectToAction("Create");
+        }
 
-        // GET: Users/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateOrganizer(NewUserDTO newUserDTO)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var user = await this.userService.CreateOrganizerAsync(newUserDTO);
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (Exception e)
+                {
+                    return BadRequest(e.Message);
+                }
+            }
+            return View();
+        }
+
         public async Task<IActionResult> Edit(string username)
         {
             var user = await this.userService.GetUserByUsernameAsync(username);
             return View(new UserDTO(user));
         }
 
-        // POST: Users/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(string username, UpdateUserDTO updateUserDTO)
@@ -96,14 +109,12 @@ namespace PhotoContest.Web.Controllers
             return View();
         }
 
-        // GET: Users/Delete/5
         public async Task<IActionResult> Delete(string username)
         {
             var user = await this.userService.GetUserByUsernameAsync(username);
             return View(new UserDTO(user));
         }
 
-        // POST: Users/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(string username)
@@ -121,11 +132,6 @@ namespace PhotoContest.Web.Controllers
                 }
             }
             return RedirectToAction("Index");
-        }
-
-        private bool UserExists(Guid id)
-        {
-            return _context.Users.Any(e => e.Id == id);
         }
     }
 }
