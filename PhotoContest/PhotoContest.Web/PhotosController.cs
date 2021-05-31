7bb412bedcdcea64591eff687c0720beb20317b3
Emil Nenchev <emilnenchev7@gmail.com>
2021-05-31 11:47:43 +0300

    Scaffold photos

---
+++ PhotoContest/PhotoContest.Web/PhotosController.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PhotoContest.Data;
+using PhotoContest.Data.Models;
+
+namespace PhotoContest.Web
+{
+    public class PhotosController : Controller
+    {
+        private readonly PhotoContestContext _context;
+
+        public PhotosController(PhotoContestContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Photos
+        public async Task<IActionResult> Index()
+        {
+            var photoContestContext = _context.Photos.Include(p => p.Contest).Include(p => p.User);
+            return View(await photoContestContext.ToListAsync());
+        }
+
+        // GET: Photos/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var photo = await _context.Photos
+                .Include(p => p.Contest)
+                .Include(p => p.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (photo == null)
+            {
+                return NotFound();
+            }
+
+            return View(photo);
+        }
+
+        // GET: Photos/Create
+        public IActionResult Create()
+        {
+            ViewData["ContestId"] = new SelectList(_context.Contests, "Id", "Name");
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Email");
+            return View();
+        }
+
+        // POST: Photos/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,Description,PhotoUrl,UserId,ContestId,AllPoints,CreatedOn,ModifiedOn,DeletedOn,IsDeleted")] Photo photo)
+        {
+            if (ModelState.IsValid)
+            {
+                photo.Id = Guid.NewGuid();
+                _context.Add(photo);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ContestId"] = new SelectList(_context.Contests, "Id", "Name", photo.ContestId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Email", photo.UserId);
+            return View(photo);
+        }
+
+        // GET: Photos/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var photo = await _context.Photos.FindAsync(id);
+            if (photo == null)
+            {
+                return NotFound();
+            }
+            ViewData["ContestId"] = new SelectList(_context.Contests, "Id", "Name", photo.ContestId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Email", photo.UserId);
+            return View(photo);
+        }
+
+        // POST: Photos/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("Id,Title,Description,PhotoUrl,UserId,ContestId,AllPoints,CreatedOn,ModifiedOn,DeletedOn,IsDeleted")] Photo photo)
+        {
+            if (id != photo.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(photo);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PhotoExists(photo.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ContestId"] = new SelectList(_context.Contests, "Id", "Name", photo.ContestId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Email", photo.UserId);
+            return View(photo);
+        }
+
+        // GET: Photos/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var photo = await _context.Photos
+                .Include(p => p.Contest)
+                .Include(p => p.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (photo == null)
+            {
+                return NotFound();
+            }
+
+            return View(photo);
+        }
+
+        // POST: Photos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var photo = await _context.Photos.FindAsync(id);
+            _context.Photos.Remove(photo);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PhotoExists(Guid id)
+        {
+            return _context.Photos.Any(e => e.Id == id);
+        }
+    }
+}
