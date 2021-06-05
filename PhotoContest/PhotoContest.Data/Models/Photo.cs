73b95fcc407c51794ad68819a060a79552feb8a9
AngelYankov <angel_yankovv@abv.bg>
2021-06-05 17:22:40 +0300

    Seed more Photos, Reviews and Contests

--- PhotoContest/PhotoContest.Data/Models/Photo.cs
+++ PhotoContest/PhotoContest.Data/Models/Photo.cs
@@ -12,10 +12,10 @@ namespace PhotoContest.Data.Models
         [Key]
         public Guid Id { get; set; }
 
-        [Required, StringLength(15, MinimumLength = 3, ErrorMessage = "Value for photo {0} should be between {2} and {1} characters.")]
+        [Required, StringLength(20, MinimumLength = 3, ErrorMessage = "Value for photo {0} should be between {2} and {1} characters.")]
         public string Title { get; set; }
 
-        [Required, StringLength(30, MinimumLength = 5, ErrorMessage = "Value for photo {0} should be between {2} and {1} characters.")]
+        [Required, StringLength(50, MinimumLength = 5, ErrorMessage = "Value for photo {0} should be between {2} and {1} characters.")]
         public string Description { get; set; }
         [Required]
         public string PhotoUrl { get; set; }
